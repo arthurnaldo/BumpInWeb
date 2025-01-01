da0f22345eb55b77aef16220e5edba617b5324b7
Atharv Naidu <atharvan.naidu@outlook.com>
2024-12-31 22:48:17 -0800

    fix: simplify path handling

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://atharvnaidu.github.io/BumpInWeb/',
+  base: '',
 })
